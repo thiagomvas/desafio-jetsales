78030d634f5f446bd38a1f48fc04eb7d6d2d2111
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-07-16 10:29:55 -0300

    fix: Fix prisma setup

--- backend/prisma/client.ts
+++ backend/prisma/client.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
-});
+const prisma = new PrismaClient();
 export default prisma;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './docs/swagger.json';
+
+import taskRoutes from './routes/task.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/tasks', taskRoutes);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/hello', (_req, res) => {
   res.json({ message: 'Hello World!' });
